63e2f54e0d28570ede74853eeb52f3cdcca52120
muh.syam <muhammad.syamsuddin@pnep.co.id>
2022-09-24 10:04:36 +0700

    membuat app-say-hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/msyam0709/app-say-hello.git
+
+go 1.18
+
+require github.com/msyam0709/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+	go_say_hello "github.com/msyam0709/go-say-hello"
+)
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
